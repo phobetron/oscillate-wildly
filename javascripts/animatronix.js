be6201210471cc267f2a2a32c30a36ce53f77b3b
Charles Hudson <chudson@attinteractive.com>
2011-04-08 17:35:27 -0700

    Some cleanup thanks to Doug McInnes

--- javascripts/animatronix.js
+++ javascripts/animatronix.js
@@ -25,130 +25,128 @@
     }
 
     var calculators = {
-      rose: function() { return {
-        t: 0,
-        x: 1,
-        y: 1,
+      rose: function() {
+        var t = 0;
+        var x = 1;
+        var y = 1;
 
-        calculate: function() {
-          var t = this.t, x = this.x, y = this.y;
-          t++;
-          x = Math.sin(2*t/180)*Math.cos(5*t/180)*(this.halfWidth/1.2);
-          y = Math.sin(2*t/180)*Math.sin(5*t/180)*(this.halfHeight/1.2);
-          this.c.translate(Math.ceil(x)+this.halfWidth, Math.ceil(y)+this.halfHeight);
-          this.t = t; this.x = x; this.y = y;
-        }
-      } },
-
-      lissajous: function() { return {
-        t: 0,
-        x: 1,
-        y: 1,
-
-        calculate: function() {
-          var t = this.t, x = this.x, y = this.y;
-          t++;
-          x = -Math.cos(t/288 * (Math.PI*2))*(this.halfWidth/1.2);
-          y = Math.sin(t/360 * (Math.PI*2))*(this.halfHeight/1.2);
-          this.c.translate(Math.ceil(x)+this.halfWidth, Math.ceil(y)+this.halfHeight);
-          this.t = t; this.x = x; this.y = y;
-        }
-      } },
-
-      ellipse: function(ellipsoid) { return {
-        t: 0,
-        x: 1,
-        y: 1,
-        z: 1,
-
-        ellipsoid: ellipsoid,
-
-        calculate: function() {
-          var t = this.t, x = this.x, y = this.y, z = this.z;
-          t++;
-          x = -Math.sin(t/360 * (Math.PI*2))*(this.halfWidth/1.2);
-          y = Math.cos(t/360 * (Math.PI*2))*(this.halfHeight/2.5);
-          this.c.translate(Math.ceil(x)+this.halfWidth, Math.ceil(y)+this.halfHeight);
-          if (this.ellipsoid) {
-            z = Math.round(Math.abs(Math.cos(t/360 * Math.PI))*100)/100;
-            this.c.scale(z+0.5, z+0.5);
+        return {
+          calculate: function() {
+            t++;
+            x = Math.sin(2*t/180)*Math.cos(5*t/180)*(this.halfWidth/1.2);
+            y = Math.sin(2*t/180)*Math.sin(5*t/180)*(this.halfHeight/1.2);
+            this.c.translate(Math.ceil(x)+this.halfWidth, Math.ceil(y)+this.halfHeight);
           }
-          this.t = t; this.x = x; this.y = y; this.z = z;
         }
-      } },
+      },
+
+      lissajous: function() {
+        var t = 0;
+        var x = 1;
+        var y = 1;
+
+        return {
+          calculate: function() {
+            t++;
+            x = -Math.cos(t/288 * (Math.PI*2))*(this.halfWidth/1.2);
+            y = Math.sin(t/360 * (Math.PI*2))*(this.halfHeight/1.2);
+            this.c.translate(Math.ceil(x)+this.halfWidth, Math.ceil(y)+this.halfHeight);
+          }
+        }
+      },
+
+      ellipse: function(ellipsoid) {
+        var t = 0;
+        var x = 1;
+        var y = 1;
+        var z = 1;
+
+        return {
+          calculate: function() {
+            t++;
+            x = -Math.sin(t/360 * (Math.PI*2))*(this.halfWidth/1.2);
+            y = Math.cos(t/360 * (Math.PI*2))*(this.halfHeight/2.5);
+            this.c.translate(Math.ceil(x)+this.halfWidth, Math.ceil(y)+this.halfHeight);
+            if (ellipsoid) {
+              z = Math.round(Math.abs(Math.cos(t/360 * Math.PI))*100)/100;
+              this.c.scale(z+0.5, z+0.5);
+            }
+          }
+        }
+      },
 
       ellipsoid: function() { return calculators.ellipse(true); },
 
-      vanderpol: function() { return {
-        t: 0,
-        x: 1,
-        y: 1,
+      vanderpol: function() {
+        var t = 0;
+        var x = 1;
+        var y = 1;
 
-        calculate: function() {
-          var t = this.t, x = this.x, y = this.y;
-          t++;
-          var h = 0.1;
-          var mu = 1.0;
-          var _x,_y;
-          _x = x+h * y;
-          _y = y+h * (-x - mu * (Math.pow(x,2) - 1) * y);
-          x = _x;
-          y = _y;
-          this.c.translate(Math.ceil(x*40)+this.halfWidth, Math.ceil(y*40)+this.halfHeight);
-          this.t = t; this.x = x; this.y = y;
+        return {
+          calculate: function() {
+            t++;
+            var h = 0.1;
+            var mu = 1.0;
+            var _x,_y;
+            _x = x+h * y;
+            _y = y+h * (-x - mu * (Math.pow(x,2) - 1) * y);
+            x = _x;
+            y = _y;
+            this.c.translate(Math.ceil(x*40)+this.halfWidth, Math.ceil(y*40)+this.halfHeight);
+          }
         }
-      } },
+      },
 
-      duffing: function() { return {
-        t: 0.0,
-        x: 0.2,
-        y: -0.3,
+      duffing: function() {
+        var t = 0.0;
+        var x = 0.2;
+        var y = -0.3;
 
-        calculate: function() {
-          var t = this.t, x = this.x, y = this.y;
-          if (t >= 500.0) t = 0.0;
-          if (x*60 > this.halfWidth+1 || x*60 < -this.halfWidth-1) x = 0;
-          if (y*50 > this.halfHeight+1 || y*50 < -this.halfHeight-1) y = 0;
-          else t += 0.1;
-          var h = 0.1;
-          var a = 0.25;
-          var b = 0.3;
-          var w = 1;
-          var _x,_y;
-          _x = x+h * y;
-          _y = y+h * (x-Math.pow(x,3)-a*y+b*Math.cos(w*t));
-          x = _x;
-          y = _y;
-          this.c.translate(Math.ceil(x*60)+this.halfWidth, Math.ceil(y*50)+this.halfHeight);
-          this.t = t; this.x = x; this.y = y;
+        return {
+          calculate: function() {
+            if (t >= 500.0) t = 0.0;
+            if (x*60 > this.halfWidth+1 || x*60 < -this.halfWidth-1) x = 0;
+            if (y*50 > this.halfHeight+1 || y*50 < -this.halfHeight-1) y = 0;
+            else t += 0.1;
+            var h = 0.1;
+            var a = 0.25;
+            var b = 0.3;
+            var w = 1;
+            var _x,_y;
+            _x = x+h * y;
+            _y = y+h * (x-Math.pow(x,3)-a*y+b*Math.cos(w*t));
+            x = _x;
+            y = _y;
+            this.c.translate(Math.ceil(x*60)+this.halfWidth, Math.ceil(y*50)+this.halfHeight);
+          }
         }
-      } },
+      },
 
-      lorenz: function() { return {
-        t: 0,
-        x: 1,
-        y: 1,
-        z: 1,
+      lorenz: function() {
+        var t = 0;
+        var x = 1;
+        var y = 1;
+        var z = 1;
 
-        calculate: function() {
-          var t = this.t, x = this.x, y = this.y, z = this.z;
-          t++;
-          var h = 0.01;
-          var b = 8.0/3.0;
-          var s = 10.0;
-          var r = 28.0;
-          var _x,_y,_z;
-          _x = x+h * (s * (y - x));
-          _y = y+h * (x * (r - z) - y);
-          _z = z+h * ((x * y) - (b * z));
-          x = _x;
-          y = _y;
-          z = _z;
-          this.c.translate(Math.ceil(x*5)+this.halfWidth, Math.ceil(y*5)+this.halfHeight);
-          this.c.scale(z/20,z/20);
-          this.t = t; this.x = x; this.y = y; this.z = z;
+        return  {
+          calculate: function() {
+            t++;
+            var h = 0.01;
+            var b = 8.0/3.0;
+            var s = 10.0;
+            var r = 28.0;
+            var _x,_y,_z;
+            _x = x+h * (s * (y - x));
+            _y = y+h * (x * (r - z) - y);
+            _z = z+h * ((x * y) - (b * z));
+            x = _x;
+            y = _y;
+            z = _z;
+            this.c.translate(Math.ceil(x*5)+this.halfWidth, Math.ceil(y*5)+this.halfHeight);
+            this.c.scale(z/20,z/20);
+          }
         }
-      } }
+      }
     }
 
     return this.each(function(i) {
